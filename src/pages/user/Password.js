a3f9d81f57d833fa2a1880ef46bf077a300ad10c
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-09-27 23:06:54 +0530

    CHG | User Password Reset and Wishlist Page

---
+++ src/pages/user/Password.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import UserNav from '../../components/nav/UserNav'
+
+const Password = () => {
+  return (
+    <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-md-2'>
+            <UserNav />
+          </div>
+            <div className='col'>
+                User Password Reset Page
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Password
