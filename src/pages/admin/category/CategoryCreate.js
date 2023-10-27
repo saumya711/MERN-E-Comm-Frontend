626373e2af7ab1cccac47fc2aa7537ffe9a408ea
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-10-27 21:28:15 +0530

    CHG | Code Refactor - search filter

--- src/pages/admin/category/CategoryCreate.js
+++ src/pages/admin/category/CategoryCreate.js
@@ -10,6 +10,7 @@ import {
 import { Link } from "react-router-dom";
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import CategoryForm from "../../../components/forms/CategoryForm"
+import LocalSearch from '../../../components/forms/LocalSearch';
 
 const CategoryCreate = () => {
   const {user} = useSelector((state) => ({...state}))
@@ -72,12 +73,6 @@ const CategoryCreate = () => {
     }
   }
 
-  // step 3
-  const handleSearchChange = (e) => {
-    e.preventDefault();
-    setKeyword(e.target.value.toLowerCase());
-  }
-
   // step 4
   const searched = (keyword) => (c) => c.name.toLowerCase().includes(keyword);
 
@@ -96,15 +91,11 @@ const CategoryCreate = () => {
                 setname={setname}
               />
 
-              {/* step 2 */}
-              <input
-                type='search'
-                placeholder='Filter'
-                value={keyword}
-                onChange={handleSearchChange}
-                className='form-control mb-4'
+              {/* step 2 and 3*/}
+              < LocalSearch 
+                keyword={keyword}
+                setKeyword={setKeyword}
               />
-              <hr />
               {/* step 5 */}
               {categories.filter(searched(keyword)).map((c) => (
                 <div className='alert alert-secondary' key={c._id}>
