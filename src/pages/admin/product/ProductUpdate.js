24b3be6180087ea50863d613cb102c7829c0e912
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-11-08 22:45:30 +0530

    CHG | Get Product on Update Page

--- src/pages/admin/product/ProductUpdate.js
+++ src/pages/admin/product/ProductUpdate.js
@@ -2,30 +2,58 @@ import React, { useState, useEffect} from 'react';
 import AdminNav from '../../../components/nav/AdminNav';
 import { toast } from 'react-toastify';
 import { useSelector } from 'react-redux';
-import { createProduct } from '../../../functions/product';
-import ProductCreateForm from '../../../components/forms/ProductCreateForm';
-import { getAllCategories, getSubCategories } from '../../../functions/category';
-import FileUpload from '../../../components/forms/FileUpload';
-import { LoadingOutlined } from '@ant-design/icons';
+import { useParams } from 'react-router-dom';
+import { getProduct } from '../../../functions/product';
 
-const ProductUpdate = () => {
+const initialState = {
+    title: "",
+    description: "",
+    price: "",
+    categories: [],
+    category: "",
+    subs: [],
+    shipping: "",
+    quantity: "",
+    images: [],
+    colors: ["Black", "Brown", "Silver", "White", "Blue"],
+    brands: ["Apple", "Samsung", "Microsoft", "Lenovo", "Asus"],
+    color: "",
+    brand: "",
+  }
 
-  const {user} = useSelector((state) => ({...state}));
+const ProductUpdate = ({ match }) => {
+    const [ values, setValues ] = useState(initialState);
+    const {user} = useSelector((state) => ({...state}));
 
-  return (
-    <div className='container-fluid'>
-        <div className='row'>
-            <div className='col-md-2'>
-            <AdminNav />
-            </div>
-            <div className='col-md-10'>
-                <h4>Product Update</h4>
-                <hr />
-                
+    //router
+    // let { slug } = useParams();
+    let { slug } = match.params;
+
+    useEffect(() => {
+        loadProduct();
+    }, []);
+
+    const loadProduct = () => {
+        getProduct(slug).then((p) => {
+            //console.log("single product", p);
+            setValues({ ...values, ...p.data})
+        })
+    }
+
+    return (
+        <div className='container-fluid'>
+            <div className='row'>
+                <div className='col-md-2'>
+                <AdminNav />
+                </div>
+                <div className='col-md-10'>
+                    <h4>Product Update</h4>
+                    <hr />
+                    {JSON.stringify(values)}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ProductUpdate
