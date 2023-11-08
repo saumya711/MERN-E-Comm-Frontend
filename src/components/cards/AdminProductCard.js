e6bbecd45aa191f8da04946e1fb13bc3a75bd2ef
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-11-08 18:06:33 +0530

    CHG | Default Image ant icons

--- src/components/cards/AdminProductCard.js
+++ src/components/cards/AdminProductCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
 
@@ -7,7 +8,7 @@ const AdminProductCard = ({product}) => {
     // destructure
     const { title, description, images } = product;
     return (
-        <Card 
+        <Card
             cover={
                 <img 
                     src={images && images.length ? images[0].url : ""}
@@ -15,8 +16,13 @@ const AdminProductCard = ({product}) => {
                     className='p-1'
                 />
             }
+
+            actions={[ 
+                <EditOutlined className='text-primary' />, 
+                <DeleteOutlined className='text-danger' />
+            ]}
         >
-            <Meta title={title} description={description} />
+            <Meta title={title} description={`${description && description.substring(0, 40)}...`} />
         </Card>
     )
 }
