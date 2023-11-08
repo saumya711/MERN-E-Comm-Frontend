659ed0bc952c772f9b06ec9be37afd170487e7e5
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-11-08 08:20:24 +0530

    CHG | Admin Products Card

--- src/functions/product.js
+++ src/functions/product.js
@@ -8,3 +8,9 @@ export const createProduct = async (product, authtoken) => {
       }}
     );
 };
+
+export const getProductsByCount = async (count) => {
+  return await axios.get(
+    `${process.env.REACT_APP_API}/products/${count}`
+  );
+};
