5a25882fdcb2f2e5222eb199fbe945b8edc99e53
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-11-16 23:57:34 +0530

    CHG | Tabs

--- src/components/cards/SingleProduct.js
+++ src/components/cards/SingleProduct.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Tabs } from 'antd';
 import { Link } from 'react-router-dom';
 import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import { Carousel } from 'react-responsive-carousel';
@@ -7,20 +7,32 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import laptop from "../../images/laptop.png";
 import ProductListItems from './ProductListItems';
 
+const { TabPane } = Tabs;
+
 const SingleProduct = ({ product }) => {
-  const { title, images } = product;
+  const { title, images, description } = product;
 
   return (
     <>
       <div className='col-md-7'>
-          {images && images.length ? (
-            <Carousel showArrows={true} autoPlay infiniteLoop>
-              {images && images.map((image) => <img src={image.url} key={image.public_id}/>)}
-            </Carousel>
-          ) : (
-            <Card cover={<img src={laptop} className="mb-3 card-image" />}></Card>
-          )}
+        {images && images.length ? (
+          <Carousel showArrows={true} autoPlay infiniteLoop>
+            {images && images.map((image) => <img src={image.url} key={image.public_id}/>)}
+          </Carousel>
+        ) : (
+          <Card cover={<img src={laptop} className="mb-3 card-image" />}></Card>
+        )}
+
+        <Tabs type='card'>
+          <TabPane tab="Description" key="1">
+            {description}
+          </TabPane>
+          <TabPane tab="More" key="2">
+            Call use on xxxx xxx xxx to learn more about this product
+          </TabPane>
+        </Tabs>
       </div>
+
       <div className='col-md-5'>
         <h1 className="bg-info p-3">{title}</h1>
         <Card
