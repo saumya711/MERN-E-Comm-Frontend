e284def6e26bb3d262984b78490dea25389368a6
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-11-16 22:40:39 +0530

    CHG | Default Image

--- src/components/home/NewArrivals.js
+++ src/components/home/NewArrivals.js
@@ -30,32 +30,31 @@ const NewArrivals = () => {
 
   return (
     <>
-    {productsCount}
-        <div className='container'>
-            {loading ? (
-            <LoadingCard count={3}/>
-            ) : (
-            <div className='row'>
-                {products.map((product) => (
-                <div key={product._id} className='col-md-4 pb-3'>
-                    < ProductCard
-                    product={product}
-                    />
-                </div>
-                ))}
-            </div>
-            )}
-        </div>
+      <div className='container'>
+          {loading ? (
+          <LoadingCard count={3}/>
+          ) : (
+          <div className='row'>
+              {products.map((product) => (
+              <div key={product._id} className='col-md-4 pb-3'>
+                  < ProductCard
+                  product={product}
+                  />
+              </div>
+              ))}
+          </div>
+          )}
+      </div>
 
-        <div className='row'>
-            <nav className='col-md-4 offset-md-4 text-center pt-2 p-3'>
-            <Pagination
-                current={page}
-                total={(productsCount / 3) * 10}
-                onChange={(value) => setPage(value)}
-            />
-            </nav>
-        </div>
+      <div className='row'>
+          <nav className='col-md-4 offset-md-4 text-center pt-2 p-3'>
+          <Pagination
+              current={page}
+              total={(productsCount / 3) * 10}
+              onChange={(value) => setPage(value)}
+          />
+          </nav>
+      </div>
     </> 
   )
 }
