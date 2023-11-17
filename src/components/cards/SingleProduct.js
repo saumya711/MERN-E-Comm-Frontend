fb359c3988ea78f5d56e211906ce242f80af9e9b
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-11-17 22:04:27 +0530

    CHG | React Star Rating

--- src/components/cards/SingleProduct.js
+++ src/components/cards/SingleProduct.js
@@ -6,11 +6,12 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import laptop from "../../images/laptop.png";
 import ProductListItems from './ProductListItems';
+import StarRatings from 'react-star-ratings';
 
 const { TabPane } = Tabs;
 
 const SingleProduct = ({ product }) => {
-  const { title, images, description } = product;
+  const { title, images, description, _id } = product;
 
   return (
     <>
@@ -35,6 +36,18 @@ const SingleProduct = ({ product }) => {
 
       <div className='col-md-5'>
         <h1 className="bg-info p-3">{title}</h1>
+        
+        <StarRatings
+          name={_id}
+          numberOfStars={5}
+          rating={2}
+          starRatedColor="red"
+          changeRating={(newRating, name) => 
+            console.log("newRating", newRating, "name", name)
+          }
+          isSelectable={true}
+        />
+
         <Card
           actions={[
             <>
