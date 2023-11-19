a7e77f11cbcfa1da0fc8adf5a774bc36d7a2b4ec
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-11-19 19:58:06 +0530

    CHG | Category Page with product

--- src/pages/category/CategoryHome.js
+++ src/pages/category/CategoryHome.js
@@ -11,16 +11,38 @@ const CategoryHome = ({ match }) => {
   const { slug } = match.params;
 
   useEffect(() => {
-    	setLoading(true);
-      getCategory(slug).then((c) => {
-        setCategory(c.data);
-        console.log(JSON.stringify(c.data, null, 4));
-      })
+    setLoading(true);
+    getCategory(slug).then((res) => {
+      setCategory(res.data.category);
+      setProducts(res.data.products)
+      console.log(JSON.stringify(res.data, null, 4));
+      setLoading(false);
+    })
   }, []);
 
   return (
-    <div>
-      {slug}
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col'>
+          {loading ? (
+            <h4 className='text-center p-3 mt-5 mb-5 display-4 jumbotron'>
+              Loading...
+            </h4>
+          ) : (
+            <h4 className='text-center p-3 mt-5 mb-5 display-4 jumbotron'>
+              {products.length} products in "{category.name}" category
+            </h4>
+          )}
+        </div>
+      </div>
+
+      <div className='row'>
+        {products.map((product) => (
+          <div className='col-md-4' key={product._id}>
+            <ProductCard product={product} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
