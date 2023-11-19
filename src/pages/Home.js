928f05a52e69139edee6aa1d19afc58efc6d2ca4
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-11-19 13:57:34 +0530

    CHG | Categories List in Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Jumbotron from '../components/cards/Jumbotron';
 import NewArrivals from '../components/home/NewArrivals';
 import BestSellers from '../components/home/BestSellers';
+import CategoryList from '../components/category/CategoryList';
 
 const Home = () => {
 
@@ -22,6 +23,12 @@ const Home = () => {
       </h4>
 
       <BestSellers />
+
+      <h4 className='text-center p-3 mb-5 mt-5 display-4 jumbotron'>
+        Category
+      </h4>
+
+      <CategoryList />
       <br />
     </> 
   )
