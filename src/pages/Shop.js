a3c7d274c2cca77f9569a6cdb42c91f285b6ced5
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-11-21 19:12:36 +0530

    CHG | Filter Products by Categories

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProductsByFilter, getProductsByCount } from '../functions/product';
+import { getAllCategories } from '../functions/category'
 import ProducCard from '../components/cards/ProductCard';
-import { Menu, Slider } from 'antd';
-import { DollarOutlined } from '@ant-design/icons';
+import { Menu, Slider, Checkbox } from 'antd';
+import { DollarOutlined, DownSquareOutlined } from '@ant-design/icons'
 
 const { SubMenu, Item } = Menu;
 
@@ -12,6 +13,8 @@ const Shop = () => {
   const [loading, setLoading] = useState(false);
   const [price, setPrice] = useState([0,0]);
   const [ok, setOk] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [categoryIds, setCategoryIds] = useState([]);
 
   const dispatch = useDispatch();
   const { search } = useSelector((state) => ({ ...state }));
@@ -19,6 +22,7 @@ const Shop = () => {
 
   useEffect(() => {
     loadAllProducts();
+    getAllCategories().then((res) => setCategories(res.data));
   }, []);
 
   const fetchProducts = (arg) => {
@@ -55,10 +59,34 @@ const Shop = () => {
       type: "SEARCH_QUERY",
       payload: { text: '' },
     });
+    setCategoryIds([]);
     setPrice(value);
     setTimeout(() => {
       setOk(!ok);
     }, 300)
+  }
+
+  // 4. load products based on category
+  const handleCheckbox = (e) => {
+    dispatch({
+      type: "SEARCH_QUERY",
+      payload: { text: '' },
+    });
+    setPrice([0, 0]);
+    let inTheState = [...categoryIds];
+    let justChecked = e.target.value;
+    let foundInTheState = inTheState.indexOf(justChecked) // index or -1
+
+    if (foundInTheState === -1) {
+      inTheState.push(justChecked);
+    } else {
+      // if found pull out one item from index
+      inTheState.splice(foundInTheState, 1)
+    }
+
+    setCategoryIds(inTheState);
+    // console.log(inTheState);
+    fetchProducts({ category: inTheState});
   }
 
   return (
@@ -69,6 +97,7 @@ const Shop = () => {
           <hr/>
 
           <Menu defaultOpenKeys={['1', '2']} mode='inline'>
+            {/* Price */}
             <SubMenu 
               key='1' 
               title={
@@ -86,6 +115,32 @@ const Shop = () => {
                   onChange={handleSlider}
                   max="5000"
                 />
+              </div>
+            </SubMenu>
+            
+            {/* category */}
+            <SubMenu 
+              key='2' 
+              title={
+                <span className='h6'>
+                  <DownSquareOutlined /> category
+                </span>
+              }
+            >
+              <div style={{ marginTop: "-10px"}}>
+                {categories.map((c) => (
+                  <div key={c._id}>
+                    <Checkbox 
+                      className='pb-2 pl-2 pr-2' 
+                      value={c._id} 
+                      name='category' 
+                      onChange={handleCheckbox}
+                      checked={categoryIds.includes(c._id)}
+                    >
+                      {c.name}
+                    </Checkbox>
+                  </div>
+                ))}
               </div>
             </SubMenu>
           </Menu>
