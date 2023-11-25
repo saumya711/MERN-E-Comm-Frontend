436c6bc1351183951171954ce89c2f5f8c8802e9
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-11-25 17:34:10 +0530

    CHG | Pick A Color in Cart Page

--- src/components/cards/ProductCardInCheckout.js
+++ src/components/cards/ProductCardInCheckout.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ModalImage from "react-modal-image";
 import laptop from "../../images/laptop.png";
+import { useDispatch } from 'react-redux';
 
 const ProductCardInCheckout = ({ product }) => {
+  const colors = ["Black", "Brown", "Silver", "White", "Blue"];
+  let dispatch = useDispatch();
+
+  const handleColorChange = (e) => {
+    console.log('Color Changed', e.target.value);
+
+    let cart = [];
+    if (typeof window !== 'undefined') {
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"))
+      }
+
+      cart.map((p, i) => {
+        if (p._id === product._id) {
+          cart[i].color = e.target.value;
+        }
+      });
+  
+      // console.log('cart update color', cart);
+      localStorage.setItem("cart", JSON.stringify(cart));
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: cart,
+      });
+    }
+  }
+  
   return (
     <tbody>
       <tr>
@@ -18,7 +46,26 @@ const ProductCardInCheckout = ({ product }) => {
         <td>{product.title}</td>
         <td>${product.price}</td>
         <td>{product.brand}</td>
-        <td>{product.color}</td>
+        <td>
+          <select 
+            onChange={handleColorChange} 
+            name='color' 
+            className='form-control'
+          >
+            {product.color ? ( 
+              <option value={product.color}>{product.color}</option> 
+            ) : ( 
+              <option>Select</option> 
+            )}
+            {colors
+              .filter((c) => c !== product.color)
+              .map((c) => (
+                <option key={c} value={c}>
+                  {c}
+                </option>
+            ))}
+          </select>
+        </td>
         <td>1</td>
         <td>shipping icon</td>
         <td>delete icon</td>
