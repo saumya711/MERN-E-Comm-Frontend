516db88f7be42e9c9cb00ffb78b2fcf8d4dd0010
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-11-30 21:23:25 +0530

    CHG | Stripe Checkout

--- src/pages/Pyament.js
+++ src/pages/Pyament.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+// load stripe outside of components render to avoid recreating stripe object on every render
+const promise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 
 const Pyament = () => {
   return (
     <div className='container p-5 tect-center'>
-      <p>Complete Your Purchase</p>
+      <h4>Complete your Purchase</h4>
+      <Elements stripe={promise}>
+        <div className='col-md-8 offset-md-2'>
+          <p>Stripe checkout component</p>
+        </div>
+      </Elements>
     </div>
   )
 }
