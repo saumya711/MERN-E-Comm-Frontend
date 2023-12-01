57e369fba24dd4fd5033bbd9beffc7c247cd4842
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-12-01 23:49:06 +0530

    CHG | Successful Payment

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import { useSelector, useDispatch } from "react-redux";
 import { createPaymentIntent } from "../functions/stripe";
+import { Link } from "react-router-dom";
 
 const StripeCheckout = ({ history }) => {
   const dispatch = useDispatch();
@@ -24,11 +25,32 @@ const StripeCheckout = ({ history }) => {
   }, []);
 
   const handleSubmit = async (e) => {
-    //
+    e.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: e.target.name.value,
+        },
+      },
+    });
+
+    if(payload.error) {
+      setError(`Payment Failed ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      console.log(JSON.stringify(payload, null, 4));
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
+    }
   };
 
   const handleChange = async (e) => {
-    //
+    setDisabled(e.empty); // disable pay button if eerors
+    setError(e.error ? e.error.message : ""); // show error message
   };
 
   const cartStyle = {
@@ -51,6 +73,10 @@ const StripeCheckout = ({ history }) => {
 
   return (
     <>
+      <p className={succeeded ? "result-message" : "result-message hidden"}>
+        Payment Successful.(" ")
+        <Link to="/user/history">See it it your purchase history</Link>
+      </p>
       <form id="payment-form" className="stripe-form" onSubmit={handleSubmit}>
         <CardElement
           id="card-element"
@@ -65,6 +91,12 @@ const StripeCheckout = ({ history }) => {
             {processing ? <div className="spinner" id="spinner"></div> : "Pay"}
           </span>
         </button>
+        <br />
+        {error && (
+          <div className="card-error" role="alert">
+            {error}
+          </div>
+        )}
       </form>
     </>
   );
