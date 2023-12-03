ed9165947cec5ced648dc2ede37c04cdb3bc7cca
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-12-03 12:08:17 +0530

    CHG | User Orders in Purchase History Page

--- src/pages/user/History.js
+++ src/pages/user/History.js
@@ -1,7 +1,24 @@
-import React from 'react'
-import UserNav from '../../components/nav/UserNav'
+import React, {useState, useEffect} from 'react'
+import UserNav from '../../components/nav/UserNav';
+import { getUserOrders } from '../../functions/user';
+import { useSelector, useDispatch } from 'react-redux';
 
 const History = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    loadUserOrders();
+  }, []);
+
+  const loadUserOrders = () => {
+    getUserOrders(user.token).then((res) => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setOrders(res.data);
+    });
+  }
+
   return (
     <div className='container-fluid'>
         <div className='row'>
