bcc33c842db18e51d48d99de0e580a66d25c86aa
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-12-03 13:01:34 +0530

    CHG | Showing Each Order's Products in Table

--- src/pages/user/History.js
+++ src/pages/user/History.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import UserNav from '../../components/nav/UserNav';
 import { getUserOrders } from '../../functions/user';
 import { useSelector, useDispatch } from 'react-redux';
+import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 
 const History = () => {
   const { user } = useSelector((state) => ({ ...state }));
@@ -19,14 +20,63 @@ const History = () => {
     });
   }
 
+  const showOrderInTable = (order) => {
+    return (
+      <table className='table table-bordered'>
+        <thead className='thead-light'>
+          <tr>
+            <th scope='col'>Title</th>
+            <th scope='col'>Price</th>
+            <th scope='col'>Brand</th>
+            <th scope='col'>Color</th>
+            <th scope='col'>Count</th>
+            <th scope='col'>Shipping</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {order.products.map((p, i) => (
+            <tr key={i}>
+              <td><b>{p.product.title}</b></td>
+              <td>${p.product.price}</td>
+              <td>{p.product.brand}</td>
+              <td>{p.color}</td>
+              <td>{p.count}</td>
+              <td>
+                {p.product.shipping === 'Yes' ? (
+                  <CheckCircleOutlined className='text-success'/>
+                ) : (
+                  <CloseCircleOutlined className='text-danger'/>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+
   return (
     <div className='container-fluid'>
         <div className='row'>
           <div className='col-md-2'>
             <UserNav />
           </div>
-            <div className='col'>
-                User Home Page
+            <div className='col text-center'>
+              <h4>
+                {orders.length > 0 ? "User Purchase Orders" : "No Purchase Orders"}
+              </h4>
+              {orders.map((order, i) => (
+                <div key={i} className='m-5 p-3 card'>
+                  <p>Show Payment Info</p>
+                  {showOrderInTable(order)}
+                  <div className='row'>
+                    <div className='col'>
+                      <p>PDF Download</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
         </div>
     </div>
